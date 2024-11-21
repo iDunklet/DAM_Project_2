82bbbfedc519f71189e311e249e04c428d438a8d
iDunklet <152725396+iDunklet@users.noreply.github.com>
2024-11-21 10:38:31 +0100

    test

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,66 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String name = nameQuestion(sc);
+        int quantity = quantityQuestion(sc);
+        String dificulty = difficultyQuestion(sc);
     }
+
+    public static String difficultyQuestion(Scanner sc) {
+        System.out.println("Choose the difficulty: Easy, Medium, or Hard");
+        sc.nextLine();
+        String difficulty = sc.nextLine();
+        Boolean loop = false;
+        while (!loop) {
+            if (difficulty == null || difficulty.isBlank()) {
+                System.out.println("Difficulty cannot be empty. Please provide a valid input.");
+            } else {
+                switch (difficulty.toLowerCase()) {
+                    case "easy":
+                        System.out.println("You chose Easy difficulty.");
+                        loop = true;
+                        break;
+                    case "medium":
+                        System.out.println("You chose Medium difficulty.");
+                        break;
+                        loop = true;
+                    case "hard":
+                        System.out.println("You chose Hard difficulty.");
+                        break;
+                        loop = true;
+                    default:
+                        System.out.println("Invalid choice. Please choose Easy, Medium, or Hard.");
+                        loop = false;
+                        break;
+                }
+            }
+        }
+        return difficulty;
+    }
+
+    private static int quantityQuestion(Scanner sc) {
+        System.out.println("How may questions would you like to answer?");
+        System.out.println("5 minimum");
+        int quantity = sc.nextInt();
+        while (quantity < 5) {
+            System.out.println("Please enter a number 5 or greater.");
+            quantity = sc.nextInt();
+        }
+        return quantity;
+    }
+
+    private static String nameQuestion(Scanner sc) {
+        System.out.println("Welcome to my quizz");
+        System.out.println("I'm glad to see you here ");
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Nice to meet you, " + name + "! Let's start our quiz.");
+        System.out.println("press any button to continue...");
+        sc.nextLine();
+        return name;
+    }
+
+
 }
